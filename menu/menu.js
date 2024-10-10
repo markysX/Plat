a660659f287fb5f8d4fb93528539e100ef164ad8
markysX <drozdmark1114@gmail.com>
2024-10-10 15:40:37 +0300

    Add files via upload

---
+++ menu/menu.js
@@ -0,0 +1,18 @@
+
+// let colors = ['yellow','green','black', 'purple', 'orange']
+
+// let color = 0
+
+// $('.btn-task1').click(
+// function () {
+  
+// $('.task1-div').eq(0).css('display', 'none')
+//   $('.task1-div').eq(4).addClass ('black')
+//   $('.task1-div').eq(5).text('black')
+//  $('.task1-div').eq(6).css('background',colors[color])
+//   color++
+  
+// }
+// )
+
+
